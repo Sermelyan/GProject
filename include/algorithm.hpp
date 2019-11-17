fa6cce5a54c13fd9f8dbedc075ed480d372ad769
alex <aleks@mail.ru>
2019-11-18 00:37:23 +0300

    add 4 tests

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -4,30 +4,25 @@
 
 #ifndef INCLUDE_ALGORITHM_HPP_
 #define INCLUDE_ALGORITHM_HPP_
-
+#include <utility>
+#include <vector>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/dijkstra_shortest_paths.hpp>
-#include <utility>
-#include <vector>
 
-class Algorithm{
+class Algorithm {
 
-public:
-
+ public:
     typedef std::size_t dotId;
     typedef std::pair<dotId, dotId> edge;
     typedef std::size_t weight;
 
-
-private:
-
-    typedef boost::adjacency_list <boost::listS, boost::vecS, boost::directedS, boost::no_property,
-            boost::property<boost::edge_weight_t, size_t> > graph_t;
+ private:
+    typedef boost::adjacency_list <boost::listS, boost::vecS, boost::directedS,
+    boost::no_property,boost::property<boost::edge_weight_t, size_t> > graph_t;
     typedef boost::graph_traits<graph_t>::vertex_descriptor vertex_descriptor;
 
-public:
-
+ public:
     Algorithm()= delete;
     explicit Algorithm(std::vector<edge> edgeArr, std::vector<weight> weightArr);
     ~Algorithm();
@@ -38,13 +33,11 @@ public:
 
     std::vector<dotId> CalcRoute(dotId A, dotId B);
 
-private:
+ private:
     void MakeGraph();
     edge * edgeArr;
     size_t edgeSize;
     weight * weightArr;
     graph_t myGraph;
-
 };
-
 #endif  //  INCLUDE_ALGORITHM_HPP_
--- test/test.cpp
+++ test/test.cpp
@@ -8,7 +8,6 @@
 
 TEST(test1, algorithm) {
 
-
     //  у нас есть места, для алгоритма место - просто его id(size_t)
     //  dotId A=0, B=1, C=2, D=3, E=4, F=5;
     enum nodes { A, B, C, D, E, F }; // id мест
@@ -18,30 +17,18 @@ TEST(test1, algorithm) {
                           "point-D", "point-E", "point-F"};
 
     std::vector<Algorithm::edge> edges; // создадим пути
-    edges.push_back(std::make_pair(C,A));
-    edges.push_back(std::make_pair(A,B));
-    edges.push_back(std::make_pair(A,E));
-    edges.push_back(std::make_pair(F,A));
-    edges.push_back(std::make_pair(B,C));
-    edges.push_back(std::make_pair(B,D));
-    edges.push_back(std::make_pair(E,C));
-    edges.push_back(std::make_pair(E,F));
-    edges.push_back(std::make_pair(D,F));
-    edges.push_back(std::make_pair(D,E));
-    edges.push_back(std::make_pair(F,B));
-
     std::vector<Algorithm::weight> weights; // создадим метрики для путей
-    weights.push_back(10);
-    weights.push_back(7);
-    weights.push_back(8);
-    weights.push_back(6);
-    weights.push_back(8);
-    weights.push_back(12);
-    weights.push_back(10);
-    weights.push_back(8);
-    weights.push_back(1);
-    weights.push_back(13);
-    weights.push_back(5);
+    edges.push_back(std::make_pair(C,A));  weights.push_back(10);
+    edges.push_back(std::make_pair(A,B));  weights.push_back(7);
+    edges.push_back(std::make_pair(A,E));  weights.push_back(8);
+    edges.push_back(std::make_pair(F,A));  weights.push_back(6);
+    edges.push_back(std::make_pair(B,C));  weights.push_back(8);
+    edges.push_back(std::make_pair(B,D));  weights.push_back(12);
+    edges.push_back(std::make_pair(E,C));  weights.push_back(10);
+    edges.push_back(std::make_pair(E,F));  weights.push_back(8);
+    edges.push_back(std::make_pair(D,F));  weights.push_back(1);
+    edges.push_back(std::make_pair(D,E));  weights.push_back(13);
+    edges.push_back(std::make_pair(F,B));  weights.push_back(5);
 
     // кол-во метрик должно быть равно количеству путей!
     ASSERT_TRUE(edges.size() == weights.size());
@@ -61,6 +48,137 @@ TEST(test1, algorithm) {
     ASSERT_TRUE(answer.at(0) == A && answer.at(1) == E && answer.at(2) == F);
 }
 
+TEST(test2, algorithm) {
+
+
+    enum nodes { A, B, C, D, E, F };
+
+    std::vector<Algorithm::edge> edges; // создадим пути
+    std::vector<Algorithm::weight> weights; // создадим метрики для путей
+    edges.push_back(std::make_pair(C,A));  weights.push_back(10);
+    edges.push_back(std::make_pair(A,B));  weights.push_back(7);
+    edges.push_back(std::make_pair(A,E));  weights.push_back(8);
+    edges.push_back(std::make_pair(F,A));  weights.push_back(6);
+    edges.push_back(std::make_pair(B,C));  weights.push_back(8);
+    edges.push_back(std::make_pair(B,D));  weights.push_back(12);
+    edges.push_back(std::make_pair(E,C));  weights.push_back(10);
+    edges.push_back(std::make_pair(E,F));  weights.push_back(8);
+    edges.push_back(std::make_pair(D,F));  weights.push_back(1);
+    edges.push_back(std::make_pair(D,E));  weights.push_back(13);
+    edges.push_back(std::make_pair(F,B));  weights.push_back(5);
+
+
+    ASSERT_TRUE(edges.size() == weights.size());
+
+    Algorithm alg = Algorithm(edges, weights);
+
+    std::vector<Algorithm::dotId> answer = alg.CalcRoute(F, C);
+    // F B C
+    ASSERT_TRUE(answer.at(0) == F && answer.at(1) == B && answer.at(2) == C);
+}
+
+TEST(test3, algorithm) {
+
+
+    enum nodes { A, B, C, D, E, F };
+
+    std::vector<Algorithm::edge> edges; // создадим пути
+    std::vector<Algorithm::weight> weights; // создадим метрики для путей
+    edges.push_back(std::make_pair(C,A));  weights.push_back(2);
+    edges.push_back(std::make_pair(A,B));  weights.push_back(1);
+    edges.push_back(std::make_pair(A,E));  weights.push_back(8);
+    edges.push_back(std::make_pair(F,A));  weights.push_back(6);
+    edges.push_back(std::make_pair(B,C));  weights.push_back(8);
+    edges.push_back(std::make_pair(B,D));  weights.push_back(3);
+    edges.push_back(std::make_pair(E,C));  weights.push_back(10);
+    edges.push_back(std::make_pair(E,F));  weights.push_back(8);
+    edges.push_back(std::make_pair(D,F));  weights.push_back(2);
+    edges.push_back(std::make_pair(D,E));  weights.push_back(13);
+    edges.push_back(std::make_pair(F,B));  weights.push_back(5);
+
+
+    ASSERT_TRUE(edges.size() == weights.size());
+
+    Algorithm alg = Algorithm(edges, weights);
+
+    std::vector<Algorithm::dotId> answer = alg.CalcRoute(C, F);
+    // C A B D F
+    ASSERT_TRUE(answer.at(0) == C && answer.at(1) == A && answer.at(2) == B
+                && answer.at(3) == D && answer.at(4) == F);
+}
+
+TEST(test4_2ways, algorithm) {
+
+    enum nodes { A, B, C, D, E, F };
+
+    std::vector<Algorithm::edge> edges; // создадим пути
+    std::vector<Algorithm::weight> weights; // создадим метрики для путей
+    edges.push_back(std::make_pair(C,A));  weights.push_back(10);
+    edges.push_back(std::make_pair(A,B));  weights.push_back(7);
+    edges.push_back(std::make_pair(A,E));  weights.push_back(8);
+    edges.push_back(std::make_pair(F,A));  weights.push_back(6);
+    edges.push_back(std::make_pair(B,C));  weights.push_back(8);
+    edges.push_back(std::make_pair(B,D));  weights.push_back(12);
+    edges.push_back(std::make_pair(E,C));  weights.push_back(10);
+    edges.push_back(std::make_pair(E,F));  weights.push_back(8);
+    edges.push_back(std::make_pair(D,F));  weights.push_back(1);
+    edges.push_back(std::make_pair(D,E));  weights.push_back(13);
+    edges.push_back(std::make_pair(F,B));  weights.push_back(5);
+
+    // we already have E C way
+    edges.push_back(std::make_pair(C,E));  weights.push_back(4);
+
+
+    ASSERT_TRUE(edges.size() == weights.size());
+
+    Algorithm alg = Algorithm(edges, weights);
+
+    std::vector<Algorithm::dotId> answer = alg.CalcRoute(B, E);
+    // B C E
+    ASSERT_TRUE(answer.at(0) == B && answer.at(1) == C && answer.at(2) == E);
+}
+
+TEST(test5_manyUse, algorithm) {
+
+    enum nodes { A, B, C, D, E, F };
+
+    std::vector<Algorithm::edge> edges; // создадим пути
+    std::vector<Algorithm::weight> weights; // создадим метрики для путей
+    edges.push_back(std::make_pair(C,A));  weights.push_back(10);
+    edges.push_back(std::make_pair(A,B));  weights.push_back(7);
+    edges.push_back(std::make_pair(A,E));  weights.push_back(8);
+    edges.push_back(std::make_pair(F,A));  weights.push_back(9);
+    edges.push_back(std::make_pair(B,C));  weights.push_back(8);
+    edges.push_back(std::make_pair(B,D));  weights.push_back(12);
+    edges.push_back(std::make_pair(E,C));  weights.push_back(10);
+    edges.push_back(std::make_pair(E,F));  weights.push_back(8);
+    edges.push_back(std::make_pair(D,F));  weights.push_back(1);
+    edges.push_back(std::make_pair(D,E));  weights.push_back(13);
+    edges.push_back(std::make_pair(F,B));  weights.push_back(5);
+
+    edges.push_back(std::make_pair(D,B));  weights.push_back(2);
+    edges.push_back(std::make_pair(B,A));  weights.push_back(5);
+
+    ASSERT_TRUE(edges.size() == weights.size());
+
+    Algorithm alg = Algorithm(edges, weights);
+
+    std::vector<Algorithm::dotId> answer = alg.CalcRoute(D, A);
+    // D B A
+    ASSERT_TRUE(answer.at(0) == D && answer.at(1) == B && answer.at(2) == A);
+
+    answer = alg.CalcRoute(F, C);
+    // F B C
+    ASSERT_TRUE(answer.at(0) == F && answer.at(1) == B && answer.at(2) == C);
+
+    answer = alg.CalcRoute(E, C);
+    // E C
+    ASSERT_TRUE(answer.at(0) == E && answer.at(1) == C);
+
+    answer = alg.CalcRoute(B, B); // senseless
+    // B
+    ASSERT_TRUE(answer.at(0) == B);
+}
 
 
 int main(int argc, char** argv) {
