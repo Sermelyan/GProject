f85dacf9c1d133d55d89330606eb7048a9b55707
alex <aleks@mail.ru>
2019-12-21 20:07:26 +0300

    delete cppcheck

--- linters/run.sh
+++ linters/run.sh
@@ -7,11 +7,7 @@ function print_header() {
     echo -e "\n***** ${1} *****"
 }
 
-print_header "RUN cppcheck"
 
-CPPCHECK="cppcheck"
-
-${CPPCHECK} src --enable=all -I include # --check-config
 
 print_header "RUN cpplint.py"
 python2.7 ./linters/cpplint/cpplint.py include/* src/*
