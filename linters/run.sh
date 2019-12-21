6aa1a15c8c11fe2cb81f614f2e64410f4b161e2c
alex <aleks@mail.ru>
2019-12-21 20:09:29 +0300

    delete cppcheck 2

--- linters/run.sh
+++ linters/run.sh
@@ -1,15 +1,1 @@
-#!/usr/bin/env bash
 
-set -e
-set -o pipefail
-
-function print_header() {
-    echo -e "\n***** ${1} *****"
-}
-
-
-
-print_header "RUN cpplint.py"
-python2.7 ./linters/cpplint/cpplint.py include/* src/*
-
-print_header "SUCCESS"
